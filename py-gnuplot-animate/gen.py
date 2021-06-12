8f7611fbf348116341b6c81a49788d1eb83e7278
William R. Ebenezer <w.ebenezer007@gmail.com>
2021-06-12 16:10:58 +0530

    Updated the code with better units and markings

--- py-gnuplot-animate/gen.py
+++ py-gnuplot-animate/gen.py
@@ -22,7 +22,7 @@ gp_suffix = ".p"
 
 pathIn= img_dir+"/"
 vid_name = 'A320.avi'
-fps = 20.0
+fps = 90
 
 # imports
 
@@ -61,6 +61,7 @@ def convert_frames_to_video(pathIn,pathOut,fps):
     files = [z for z in os.listdir(pathIn) if isfile(join(pathIn, z))]
     #for sorting the file names properly
     files.sort(key = lambda y: int(y[4:-4]))
+    files = files[1::5]
     for i in range(len(files)):
         filename=pathIn + files[i]
         #reading each files
@@ -89,10 +90,11 @@ for key, value in data.items():
 print(size-1, "entries found")
 count = 0
 for i in range(1, size):
+    if (i%5): continue
     fname = gnuplot_dir + "/" + gp_prefix + str(i) + gp_suffix
     fhand = open(fname, "w")
     fhand.write("""
-set terminal png font \"./lmroman10-regular.otf, 30\" size 2048, 2048
+set terminal pngcairo enhanced font \"LM-Roman-10, 30\" size 2048, 2048
 
 set tmargin 1
 set bmargin 1
@@ -129,30 +131,52 @@ set tics out nomirror
 unset xtics
 
 unset xlabel
-set ylabel \"Altitude (m)\"
-set yrange [0:12000]
-set xrange [0:350000]
-set ytic 0,2000,10000
+set ylabel \"Altitude (km)\"
+set yrange [0:14]
+set xrange [0:390]
+set ytic 2,2,12
+set arrow 1 from 0,1.524 to 390,1.524 nohead dt 2
+set arrow 2 from 0,4.572 to 390,4.572 nohead dt 2
+set arrow 3 from 0,7.3152 to 390,7.3152 nohead dt 2
+set arrow 4 from 0,10.6680 to 390,10.6680 nohead dt 2
+
+set label 1 "I" at 350,0.9 center tc rgb "gray"
+set label 2 "II" at 350,3 center tc rgb "gray"
+set label 3 "III" at 350,6 center tc rgb "gray"
+set label 4 "IV" at 350,9 center tc rgb "gray"
+set label 5 "V" at 350,12 center tc rgb "gray"
+
 plot \"<join """ + data["h"] +" "+ data["d"] + """\" using 3:2 every ::1::""" + str(i) + """ w l ls 2
 
+unset arrow 1
+unset arrow 2
+unset arrow 3
+unset arrow 4
+
+unset label 1
+unset label 2
+unset label 3
+unset label 4
+unset label 5
+
 unset xlabel
-set ylabel \"TAS (m/s)\"
-set yrange [90:260]
-set xrange [0:350000]
-set ytic 0,20,240
+set ylabel \"TAS (km/h)\"
+set yrange [200:950]
+set xrange [0:390]
+set ytic 300,200,900
 plot \"<join """ + data["TAS"] +" "+ data["d"] + """\" using 3:2 every ::1::""" + str(i) + """ w l ls 3
 
 unset xlabel
-set ylabel \"IAS (m/s)\"
-set yrange [80:180]
-set xrange [0:350000]
-set ytic 0,10,170
+set ylabel \"IAS (km/h)\"
+set yrange [200:650]
+set xrange [0:390]
+set ytic 200,100,600
 plot \"<join """ + data["IAS"] +" "+ data["d"] + """\" using 3:2 every ::1::""" + str(i) + """ w l ls 4
 
-set xlabel \"Distance (m)\"
+set xlabel \"Distance (km)\"
 set ylabel \"Mach Number\"
 set yrange [0.25:0.9]
-set xrange [0:350000]
+set xrange [0:390]
 set ytic 0.2,0.1,0.8
 set xtics nomirror
 plot \"<join """ + data["M"] +" "+ data["d"] + """\" using 3:2 every ::1::""" + str(i) + """ w l ls 5
@@ -160,6 +184,8 @@ plot \"<join """ + data["M"] +" "+ data["d"] + """\" using 3:2 every ::1::""" + 
 unset multiplot
     """)
     fhand.close()
+
+    run_script("gnuplot " + fname);
 
 # Plotting
 convert_frames_to_video(pathIn, vid_name, fps)
